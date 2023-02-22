8400f0e0650b88608de208939107d44bda643afd
John <trj@le.ac.uk>
2023-02-22 15:28:03 +0000

    Addition of scaled PCA and a new option

--- code/feature_selection.R
+++ code/feature_selection.R
@@ -5,6 +5,7 @@
 # Selected probes vs First few PCs vs Selected PCs
 #
 # Date: 19 Feb 2023
+#       22 Feb 2023 .. added the scaled PCA results
 #
 library(tidyverse)
 library(magrittr)
@@ -34,6 +35,14 @@ readRDS(file.path(home, "data/rData/subset_train_pca_scores.rds")) %>%
 readRDS(file.path(home, "data/rData/subset_valid_pca_scores.rds")) %>%
   left_join( subjDF %>%
                select(id, diagnosis, class), by = "id") -> scoreValidDF 
+
+readRDS(file.path(home, "data/rData/subset_train_scaled_pca_scores.rds")) %>%
+  left_join( subjDF %>%
+               select(id, diagnosis, class), by = "id") -> scoreTrainScaledDF 
+
+readRDS(file.path(home, "data/rData/subset_valid_scaled_pca_scores.rds")) %>%
+  left_join( subjDF %>%
+               select(id, diagnosis, class), by = "id") -> scoreValidScaledDF 
 
 # =======================================================
 # Selected probes
@@ -91,11 +100,11 @@ pbSelDF %>%
   geom_text( x=35, y=0.90, label="Out-of-sample", colour="red") 
   
 # =======================================================
-# Selected Principal Components
+# Selected Unscaled Principal Components
 #
 # PC names
 #
-pcNames <- paste0("PC", 1:350)
+pcNames <- paste0("PC", 1:375)
 
 # ------------------------------------------------
 # Loss from univariate logistic regressions
@@ -147,7 +156,7 @@ pcSelDF %>%
 
 
 # =======================================================
-# Initial Principal Components
+# Initial Unscaled Principal Components
 #
 
 # ------------------------------------------------------------------
@@ -169,32 +178,151 @@ pcOrdDF %>%
   geom_text( x=35, y=0.58, label="In-sample", colour="blue") +
   geom_text( x=35, y=1.00, label="Out-of-sample", colour="red")
 
+# =======================================================
+# Selected Scaled Principal Components
+#
+# PC names
+#
+pcNames <- paste0("PC", 1:375)
+
+# ------------------------------------------------
+# Loss from univariate logistic regressions
+#
+pcScdUniDF <- uni_logistic(scoreTrainScaledDF, "class", pcNames)
+
+# ----------------------------------------------------------
+# Plot of the 5 best PCs in training and validation data
+#
+pcScdUniDF %>% arrange(loss) %>% print() %>% slice(1:5) %>% pull(x) -> topX
+
+scoreTrainScaledDF %>%
+  pivot_longer( starts_with("PC"), names_to="pc", values_to="expression") %>%
+  filter( pc %in% topX ) %>%
+  ggplot( aes(x=pc, y=expression, fill=diagnosis)) +
+  geom_boxplot() +
+  coord_flip() +
+  labs( title = "Training Data: 5 most predictive Scaled PCs",
+        x = "") +
+  theme( legend.position = c(0.15, 0.85))
+
+scoreValidScaledDF %>%
+  pivot_longer( starts_with("PC"), names_to="pc", values_to="expression") %>%
+  filter( pc %in% topX ) %>%
+  ggplot( aes(x=pc, y=expression, fill=diagnosis)) +
+  geom_boxplot() +
+  coord_flip() +
+  labs( title = "Validation Data: 5 most predictive Scaled PCs",
+        x = "") +
+  theme( legend.position = c(0.15, 0.85))
+
+# ------------------------------------------------------------------
+# Multivariable Logistic using 1 to 50 selected PCs
+#
+pcScdSelDF <- sel_logistic(scoreTrainScaledDF, scoreValidScaledDF, 
+                           "class", pcScdUniDF, maxSel = 50)
+
+# ------------------------------------------------------------------
+# Plot of in-sample vs out-of-sample loss
+#
+pcScdSelDF %>%
+  ggplot( aes(x = n, y = inloss)) +
+  geom_line( size=1.2, colour="blue") +
+  geom_line( aes(y = outloss), size=1.2, colour="red") +
+  labs( title = "Loss from Selected Scaled PCs",
+        y     = "Loss",
+        x     = "Number of PCs") +
+  geom_text( x=35, y=0.58, label="In-sample", colour="blue") +
+  geom_text( x=35, y=2.00, label="Out-of-sample", colour="red")
+
+
+# =======================================================
+# Initial Scaled Principal Components
+#
+
+# ------------------------------------------------------------------
+# Multivariable Logistic using 1 to 50 ordered PCs
+#
+pcScdOrdDF <- sel_logistic(scoreTrainScaledDF, scoreValidScaledDF, 
+                           "class", pcScdUniDF, maxSel = 50, sort = FALSE)
+
+# ------------------------------------------------------------------
+# Plot of in-sample vs out-of-sample loss
+#
+pcScdOrdDF %>%
+  ggplot( aes(x = n, y = inloss)) +
+  geom_line( size=1.2, colour="blue") +
+  geom_line( aes(y = outloss), size=1.2, colour="red") +
+  labs( title = "Loss from Ordered Scaled PCs",
+        y     = "Loss",
+        x     = "Number of PCs") +
+  geom_text( x=35, y=0.58, label="In-sample", colour="blue") +
+  geom_text( x=35, y=1.00, label="Out-of-sample", colour="red")
 
 # ---------------------------------------------
-# Plot in-sample loss for the three methods
+# Plot in-sample loss for the five methods
 #
-pcSelDF %>%
-  rename( pcSel = inloss) %>%
-  left_join( pbSelDF %>% rename( probe  = inloss), by = "n") %>%
+pbSelDF %>%
+  rename( probe = inloss) %>%
+  left_join( pcSelDF %>% rename( pcSel  = inloss), by = "n") %>%
   left_join( pcOrdDF %>% rename( pcOrd = inloss), by = "n") %>%
-  pivot_longer(c("probe", "pcSel", "pcOrd"), names_to = "method", values_to = "loss") %>%
+  left_join( pcScdSelDF %>% rename( pcScdSel  = inloss), by = "n") %>%
+  left_join( pcScdOrdDF %>% rename( pcScdOrd = inloss), by = "n") %>%
+  pivot_longer(c("probe", "pcSel", "pcOrd", "pcScdSel", "pcScdOrd"), 
+               names_to = "method", values_to = "loss") %>%
   mutate( source = str_replace(method, "loss", ""))  %>%
   ggplot( aes(x = n, y = loss, colour=method)) +
   geom_line( size=1.2) +
-  labs( title = "In-sample loss for the 3 feature selection methods",
+  labs( title = "In-sample loss for the 5 feature selection methods",
         y     = "Loss",
         x     = "Number of predictors") +
   theme( legend.position = c(0.15, 0.25))
 
-pcSelDF %>%
-  rename( pcSel = outloss) %>%
-  left_join( pbSelDF %>% rename( probe = outloss), by = "n") %>%
+pbSelDF %>%
+  rename( probe = inloss) %>%
+  left_join( pcSelDF %>% rename( pcSel  = inloss), by = "n") %>%
+  left_join( pcOrdDF %>% rename( pcOrd = inloss), by = "n") %>%
+  left_join( pcScdSelDF %>% rename( pcScdSel  = inloss), by = "n") %>%
+  left_join( pcScdOrdDF %>% rename( pcScdOrd = inloss), by = "n") %>%
+  pivot_longer(c("probe", "pcSel", "pcOrd", "pcScdSel", "pcScdOrd"), 
+               names_to = "method", values_to = "loss") %>%
+  mutate( source = str_replace(method, "loss", ""))  %>%
+  filter( n <= 8 ) %>%
+  ggplot( aes(x = n, y = loss, colour=method)) +
+  geom_line( size=1.2) +
+  labs( title = "In-sample loss for the 5 feature selection methods",
+        y     = "Loss",
+        x     = "Number of predictors") +
+  theme( legend.position = c(0.15, 0.25))
+
+pbSelDF %>%
+  rename( probe = outloss) %>%
+  left_join( pcSelDF %>% rename( pcSel = outloss), by = "n") %>%
   left_join( pcOrdDF %>% rename( pcOrd = outloss), by = "n") %>%
-  pivot_longer(c("probe", "pcSel", "pcOrd"), names_to = "method", values_to = "loss") %>%
+  left_join( pcScdSelDF %>% rename( pcScdSel = outloss), by = "n") %>%
+  left_join( pcScdOrdDF %>% rename( pcScdOrd = outloss), by = "n") %>%
+  pivot_longer(c("probe", "pcSel", "pcOrd", "pcScdSel", "pcScdOrd"), 
+               names_to = "method", values_to = "loss") %>%
   mutate( source = str_replace(method, "loss", ""))  %>%
   ggplot( aes(x = n, y = loss, colour=source)) +
   geom_line( size=1.2) +
-  labs( title = "Out-of-sample loss for the 3 feature selection methods",
+  labs( title = "Out-of-sample loss for the 5 feature selection methods",
+        y     = "Loss",
+        x     = "Number of predictors") +
+  theme( legend.position = c(0.15, 0.85))
+
+pbSelDF %>%
+  rename( probe = outloss) %>%
+  left_join( pcSelDF %>% rename( pcSel = outloss), by = "n") %>%
+  left_join( pcOrdDF %>% rename( pcOrd = outloss), by = "n") %>%
+  left_join( pcScdSelDF %>% rename( pcScdSel = outloss), by = "n") %>%
+  left_join( pcScdOrdDF %>% rename( pcScdOrd = outloss), by = "n") %>%
+  pivot_longer(c("probe", "pcSel", "pcOrd", "pcScdSel", "pcScdOrd"), 
+               names_to = "method", values_to = "loss") %>%
+  mutate( source = str_replace(method, "loss", ""))  %>%
+  filter( n <= 8 ) %>%
+  ggplot( aes(x = n, y = loss, colour=source)) +
+  geom_line( size=1.2) +
+  labs( title = "Out-of-sample loss for the 5 feature selection methods",
         y     = "Loss",
         x     = "Number of predictors") +
   theme( legend.position = c(0.15, 0.85))
@@ -204,5 +332,7 @@ pcSelDF %>%
 #
 saveRDS( list( pcUniDF = pcUniDF, pcSelDF = pcSelDF, 
                pcOrdDF = pcOrdDF, 
-               pbUniDF = pbUniDF, pbSelDF = pbSelDF),
+               pbUniDF = pbUniDF, pbSelDF = pbSelDF,
+               pcScdUniDF = pcScdUniDF, pcScdSelDF = pcScdSelDF, 
+               pcScdOrdDF = pcScdOrdDF),
          file.path(home, "data/dataStore/classification_loss.rds"))
--- code/pca.R
+++ code/pca.R
@@ -17,6 +17,10 @@ validDF <- readRDS( file.path(home, "data/rData/validation.rds"))
 trainDF <- readRDS( file.path(home, "data/rData/training.rds"))
 subjDF  <- readRDS( file.path(home, "data/rData/subjects.rds"))
 
+# =======================================================
+# UNSCALED PRCOMP .. PCA of Covariance
+#
+
 # --------------------------------------------
 # pca of the training data
 #
@@ -45,6 +49,97 @@ predict(pca, newdata=validDF) %>%
   relocate(id) %>%
   print() %T>%   
   saveRDS(file.path(home, "data/rData/subset_valid_pca_scores.rds")) -> scoreValidDF
+
+# ---------------------------------------------
+# Principal Components Stdev (root eigenvalues)
+#
+tibble( pc = 1:375,
+        sd = pca$sdev) %>%
+  ggplot( aes(x = pc, y = sd, xend = pc, yend = 0)) +
+  geom_segment() +
+  labs( title = "Standard deviatons of the Principal Components",
+        y     = "Standard deviation",
+        x     = "Principal Component Number")
+
+# ---------------------------------------------
+# Principal Components Percent Variance
+#
+tibble( pc = 1:375,
+        sd = pca$sdev) %>%
+  mutate( pct = 100 * sd * sd / sum(sd * sd)) %>%
+  ggplot( aes(x = pc, y = pct, xend = pc, yend = 0)) +
+  geom_segment() +
+  labs( title = "Standard deviatons of the Principal Components",
+        y     = "Percent Variance",
+        x     = "Principal Component Number")
+
+
+# ---------------------------------------------
+# Illustrative plot of first 5 PCs by diagnosis
+# training data
+#
+scoreTrainDF %>%
+  select( id, PC1:PC5) %>%
+  pivot_longer(starts_with("PC"), names_to = "PC", 
+               values_to = "score") %>%
+  left_join( subjDF %>% 
+               select(id, diagnosis), by = "id") %>%
+  ggplot( aes(x = PC, y = score, fill = diagnosis)) +
+  geom_boxplot() +
+  labs(title = "Training data: PCA Scores 1 to 5 by diagnosis",
+       y     = "Score",
+       x     = "Principal Component") +
+  theme( legend.position = c(.85, .9))
+
+# ---------------------------------------------
+# Illustrative plot of first 5 PCs by diagnosis
+# validation data
+#
+scoreValidDF %>%
+  select( id, PC1:PC5) %>%
+  pivot_longer(starts_with("PC"), names_to = "PC", 
+               values_to = "score") %>%
+  left_join( subjDF %>% 
+               select(id, diagnosis), by = "id") %>%
+  ggplot( aes(x = PC, y = score, fill = diagnosis)) +
+  geom_boxplot() +
+  labs(title = "Validation data: PCA Scores 1 to 5 by diagnosis",
+       y     = "Score",
+       x     = "Principal Component") +
+  theme( legend.position = c(.85, .9))
+
+# =======================================================
+# SCALED PRCOMP .. PCA of Correlations
+#
+
+# --------------------------------------------
+# pca of the training data
+#
+trainDF %>%
+  select( -id) %>%
+  as.matrix() %>% 
+  prcomp(scale=TRUE) %T>%
+  saveRDS( file.path(home, "data/rData/subset_scaled_pca.rds")) -> pca
+
+# --------------------------------------------
+# pca scores of the training data
+#
+predict(pca) %>%
+  as_tibble() %>%
+  mutate( id = trainDF$id ) %>%
+  relocate(id) %>%
+  print() %T>%   
+  saveRDS(file.path(home, "data/rData/subset_train_scaled_pca_scores.rds")) -> scoreTrainDF
+
+# --------------------------------------------
+# pca scores of the validation data
+#
+predict(pca, newdata=validDF) %>%
+  as_tibble() %>%
+  mutate( id = validDF$id ) %>%
+  relocate(id) %>%
+  print() %T>%   
+  saveRDS(file.path(home, "data/rData/subset_valid_scaled_pca_scores.rds")) -> scoreValidDF
 
 # ---------------------------------------------
 # Principal Components Stdev (root eigenvalues)
